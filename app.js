10ba30442f98db7c37e5c47ad00a5e7ec18ae413
Sakina <64282459+sakinabehbudova@users.noreply.github.com>
2021-07-07 17:59:06 +0400

    Update app.js

--- app.js
+++ app.js
@@ -15,3 +15,40 @@ function closeNav() {
   document.getElementById("myNav").style.width = "0%";
 }
 
+/*slider our team*/
+
+$('.product-carousel').slick({
+  lazyLoad: 'ondemand',
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  nextArrow: '<i class="arrow right">',
+  prevArrow: '<i class="arrow left">',
+  infinite: false,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
