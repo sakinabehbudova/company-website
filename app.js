9ec115a588692ca225f51e56ae9d90c358c1ec49
Sakina <64282459+sakinabehbudova@users.noreply.github.com>
2021-07-16 17:12:48 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,54 +1,45 @@
 // Dark Mode
-const checkbox = document.getElementById("checkbox");
-checkbox.addEventListener("change",()=>{
-  //change the theme of the website
-  document.body.classList.toggle("dark");
-})
+
+$('#checkbox').on('change', function() {
+
+    localStorage.clear();
+
+    if ($('#checkbox').prop('checked')) {
+        localStorage.setItem('class', 'dark')
+        $('body').addClass('dark')
+        $('#checkbox').prop('checked', true);
+
+    } else {
+        $('body').removeClass('dark');
+        localStorage.setItem('class', '')
+        $('#checkbox').prop('checked', false);
+    }
+
+});
+
+
+
+if (localStorage.getItem('class') == 'dark') {
+    $('#checkbox').prop('checked', true);
+
+} else {
+    $('#checkbox').prop('checked', false);
+
+}
+
+let storg = localStorage.getItem('class');
+
+$('body').addClass(storg)
+
+
+
 
 /* Open when someone clicks on the span element */
 function openNav() {
-  document.getElementById("myNav").style.width = "100%";
+    document.getElementById("myNav").style.width = "100%";
 }
 
 /* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
-  document.getElementById("myNav").style.width = "0%";
+    document.getElementById("myNav").style.width = "0%";
 }
-
-/*slider our team*/
-
-$('.product-carousel').slick({
-  lazyLoad: 'ondemand',
-  slidesToShow: 4,
-  slidesToScroll: 1,
-  nextArrow: '<i class="arrow right">',
-  prevArrow: '<i class="arrow left">',
-  infinite: false,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        infinite: true,
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ]
-});
